94c3d51642ec5477a459baf3b8ffb159bf182925
Ankit Kumar <ankit.kr0048@gmail.com>
2025-03-25 22:09:49 +0530

    completed the basic UI

--- app/page.js
+++ app/page.js
@@ -1,13 +1,9 @@
+import CodeAnalyzer from "./components/CodeAnalyzer";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl font-bold text-center text-blue-500">
-        Hello, Tailwind CSS!
-      </h1>
-      <p className="text-center text-gray-600">
-        This is a simple Tailwind CSS example.
-        </p>
+      <CodeAnalyzer/>
     </>
   );
 }
